c25dee818d19e5d7d9b621ae1bae74ec4193494b
Dao Hoang Son <daohoangson@gmail.com>
2024-05-19 10:31:29 +0700

    Use gcr image for api-stable

--- fwfh.sh
+++ fwfh.sh
@@ -15,3 +15,10 @@ gcloud builds submit . \
 # Optional: update sample or FWFH packages
 ( cd pkgs/dart_services && dart run grinder update-pub-dependencies )
 ( cd pkgs/samples && dart tool/samples.dart )
+
+# Build API
+export COMMIT_SHA=$( git rev-parse --short HEAD )
+gcloud builds submit . \
+  --config .cloud_build/dart_services.yaml \
+  --substitutions=_FLUTTER_CHANNEL=stable,_SERVICE_NAME=api-stable,_STORAGE_BUCKET=fwfh-nnbd_artifacts,COMMIT_SHA=$COMMIT_SHA \
+  --project=$GCP_PROJECT
